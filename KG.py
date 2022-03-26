caf57af653bc1c67af46231ce53fec9b35ba3e89
jackprator <jackprator@gmail.com>
2022-03-25 21:39:31 -0400

    deploy app

---
+++ KG.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from modules import convert_to_dict, make_ordinal
+
+app = Flask(__name__)
+application = app
+
+# call Bootstrap
+Bootstrap(app)
+
+# create a list of dicts from a CSV
+album_list = convert_to_dict("KGAlbums.csv")
+
+# create a list of tuples in which the first item is the number
+# (Presidency) and the second item is the name (President)
+pairs_list = []
+for p in album_list:
+    pairs_list.append( (p['Album'], p['Title']) )
+
+# first route
+
+@app.route('/')
+def index():
+    return render_template('index.html', pairs=pairs_list, the_title="Album Index")
+
+# second route
+
+@app.route('/album/<num>')
+def detail(num):
+    try:
+        album_dict = album_list[int(num) - 1]
+    except:
+        return f"<h1>Invalid value for album: {num}</h1>"
+    # a little bonus function, imported on line 2 above
+    ord = make_ordinal( int(num) )
+    return render_template('album.html', album=album_dict, ord=ord, the_title=album_dict['Title'])
+
+
+# keep this as is
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ modules.py
@@ -0,0 +1,40 @@
+"""
+functions to be imported into Flask app
+"""
+
+import csv
+
+def convert_to_dict(filename):
+    """
+    Convert a CSV file to a list of Python dictionaries
+    """
+    # open a CSV file - note - must have column headings in top row
+    datafile = open(filename, newline='')
+
+    # create DictReader object
+    my_reader = csv.DictReader(datafile)
+
+    # create a regular Python list containing dicts
+    list_of_dicts = list(my_reader)
+
+    # close original csv file
+    datafile.close()
+
+    # return the list
+    return list_of_dicts
+
+
+def make_ordinal(num):
+    """
+    Create an ordinal (1st, 2nd, etc.) from a number.
+    """
+    base = num % 10
+    if base in [0,4,5,6,7,8,9] or num in [11,12,13]:
+        ext = "th"
+    elif base == 1:
+        ext = "st"
+    elif base == 2:
+        ext = "nd"
+    else:
+        ext = "rd"
+    return str(num) + ext
